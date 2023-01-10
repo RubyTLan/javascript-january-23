1e70d84707eb106f5101f1fb3fd51f493f9a19e1
rubenocasio <108745971+rubenocasio@users.noreply.github.com>
2023-01-09 19:20:19 -0500

    Create selectionSort.jsx

---
+++ w1/w1d2/selectionSort.jsx
@@ -0,0 +1,64 @@
+/*
+    Notes:
+    Iteration 1: Set 9 as the current minimum, then iterate through the remaining unsorted elements to find the true minimum.
+    if element < currentMinimum set element as new current minimum then keep moving down the line
+    swap minimum with first unsorted position
+
+    Iteration 2: Set 2 as the current minimum, then iterate through the remaining unsorted elements to find the true minimum.
+*/
+
+/* 
+    https://visualgo.net/en/sorting
+        
+    Selection sort works by iterating through the list, finding the minimum
+    value, and moving it to the beginning of the list. Then, ignoring the first
+    position, which is now sorted, iterate through the list again to find the
+    next minimum value and move it to index 1. This continues until all values
+    are sorted.
+    Unstable sort.
+    
+    Time Complexity
+        - Best: O(n^2) quadratic.
+        - Average: O(n^2) quadratic.
+        - Worst: O(n^2) quadratic.
+    Space: O(1) constant.
+    Selection sort is one of the slower sorts.
+    - ideal for: pagination, where page 1 displays 10 records for example,
+        you run selection sort for 10 iterations only to display the first 10
+        sorted items.
+*/
+
+// const numsOrdered = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const numsRandomOrder = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
+// const numsReversed = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+// const expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+/**
+ * Sorts given array in-place.
+ * Best: O(n^2) quadratic.
+ * Average: O(n^2) quadratic.
+ * Worst: O(n^2) quadratic.
+ * @param {Array<number>} nums
+ * @returns {Array<number>} The given array after being sorted.
+ */
+
+/*****************************************************************************/
+
+/**
+ * Sorts given array in-place.
+ * Best: O(n^2) quadratic.
+ * Average: O(n^2) quadratic.
+ * Worst: O(n^2) quadratic.
+ * @param   {Array<number>} nums
+ * @return  {Array<number>} The given array after being sorted.
+ */
+const selectionSort = (nums = []) => {
+    // Code goes here
+}
+const numsOrdered = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const numsRandomOrder = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
+const numsReversed = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+const expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(selectionSort(numsRandomOrder));
+
+module.exports = { selectionSort };
